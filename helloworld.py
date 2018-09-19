d1213e243b239425941f6ea9d47622ac39291e53
sureshpuram <puram.suresh@gmail.com>
2018-09-19 00:16:03 -0400

    finally

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,6 @@
 import webbrowser
-a_website = "https://sureshpuram.github.io/traviscifinal/"
-webbrowser.get('chrome').open_new(a_website)
-print("Hello World")
+print("started")
+url = 'https://sureshpuram.github.io/traviscifinal/'
+webbrowser.open(url)
+#webbrowser.get('chrome').open_new(a_website)
+print("completed!!")
