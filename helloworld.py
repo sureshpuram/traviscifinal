98bb3969fb320eb44f92de91de35afcc9d037077
sureshpuram <puram.suresh@gmail.com>
2018-09-19 01:08:12 -0400

    research

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,5 @@
 import webbrowser
 print("started")
-#webbrowser.get("C:/Program Files(x86)/Google/Chrome/Application/chrome.exe %s").open("https://sureshpuram.github.io/traviscifinal/")
 url = 'https://sureshpuram.github.io/traviscifinal/'
 webbrowser.open_new(url)
 #webbrowser.get('chrome').open_new(a_website)
